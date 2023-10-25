7c05081530d3bcaff8ef4c96b56f4bf82d24003c
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-10-26 01:01:38 +0530

    Change the order of ouptuts

--- bench.sh
+++ bench.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-git clean -dfx
-
-echo "RUN lfortran"
-time make -f Makefile.manual F90=lfortran F90FLAGS="-I../../src"
-cd tests/lda
-time ./conv_lda
-cd ../rlda
-time ./conv_rlda
-cd ../..
-
-echo "RUN lfortran --fast"
-git clean -dfx
-time make -f Makefile.manual F90="lfortran --skip-pass=inline_function_calls,fma --fast" F90FLAGS="-I../../src"
-cd tests/lda
-time ./conv_lda
-cd ../rlda
-time ./conv_rlda
-cd ../..
-
 
 echo "RUN gfortran"
 git clean -dfx
@@ -29,9 +10,27 @@ cd ../rlda
 time ./conv_rlda
 cd ../..
 
+echo "RUN lfortran"
+git clean -dfx
+time make -f Makefile.manual F90=lfortran F90FLAGS="-I../../src"
+cd tests/lda
+time ./conv_lda
+cd ../rlda
+time ./conv_rlda
+cd ../..
+
 echo "RUN gfortran -fast"
 git clean -dfx
 time make -f MakefileRelease.manual
+cd tests/lda
+time ./conv_lda
+cd ../rlda
+time ./conv_rlda
+cd ../..
+
+echo "RUN lfortran --fast"
+git clean -dfx
+time make -f Makefile.manual F90="lfortran --skip-pass=inline_function_calls,fma --fast" F90FLAGS="-I../../src"
 cd tests/lda
 time ./conv_lda
 cd ../rlda
